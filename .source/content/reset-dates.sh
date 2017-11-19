d88b011a03badbc6176d807bb6f6ef311c8f790b
peter <petermiller1986@gmail.com>
2017-11-20 08:42:51 +1030

    use absolute urls in production

---
+++ .source/content/reset-dates.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+touch -mt "201711192331.47" example-article1/index.md
+touch -mt "201711192332.04" example-article2/index.md
+touch -mt "201711192332.15" example-article3/index.md
+touch -mt "201711192332.33" example-article4/index.md
+touch -mt "201711192332.44" example-article5/index.md
+touch -mt "201711192332.56" example-article6/index.md
--- .source/publishconf.py
+++ .source/publishconf.py
@@ -10,13 +10,13 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+SITEURL = 'http://inquisitive-ibis.null.place'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
