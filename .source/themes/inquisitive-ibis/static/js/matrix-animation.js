511c891f8ff209c9ea0a4953b75027dcd88dee04
peter <petermiller1986@gmail.com>
2017-11-23 10:03:56 +1030

    fix the css and use a new shattered-glass img in front of the matrix animation

--- .source/themes/inquisitive-ibis/static/js/matrix-animation.js
+++ .source/themes/inquisitive-ibis/static/js/matrix-animation.js
@@ -1,13 +1,13 @@
-// code to run a matrix-lookalike in a <canvas>
+// code to run a matrix-movie animation in a <canvas>
 
 // we use a lot of variables between functions, so keep them out of the global
 // scope with a closure
 (function() {
     // init vars
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var canvas2 = document.getElementById('canvas2');
-    var ctx2 = canvas2.getContext('2d');
+    var green_text_canvas = document.getElementById('matrix_text_green');
+    var green_text_ctx = green_text_canvas.getContext('2d');
+    var white_text_canvas = document.getElementById('matrix_text_white');
+    var white_text_ctx = white_text_canvas.getContext('2d');
     var cw = 300;
     var ch = 200;
     var charArr = ['杕','丁','丂','七','丄','丅','丆','万','丈','三','上','下','丌','不','与','丏','丐','丑','丒','专','且','丕','世','丗','丘','丙','业'];
@@ -19,12 +19,12 @@
     var maxColums = cw/(fontSize);
 
     // init each canvas
-    canvas.width = canvas2.width = cw;
-    canvas.height = canvas2.height = ch;
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, cw, ch);
-    ctx2.fillStyle = 'black';
-    ctx2.fillRect(0, 0, cw, ch);
+    green_text_canvas.width = white_text_canvas.width = cw;
+    green_text_canvas.height = white_text_canvas.height = ch;
+    green_text_ctx.fillStyle = 'black';
+    green_text_ctx.fillRect(0, 0, cw, ch);
+    white_text_ctx.fillStyle = 'black';
+    white_text_ctx.fillRect(0, 0, cw, ch);
 
     function RandomInt(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
@@ -46,7 +46,7 @@
         this.y = y;
     }
 
-    Point.prototype.draw = function(ctx) {
+    Point.prototype.draw = function() {
         //randomly skip 1/10 of the columns every frame
         if (RandomFloat(0, 10) < 1) return;
 
@@ -54,13 +54,13 @@
         if (this.speed == null) this.speed = RandomFloat(1, 10);
         if (this.maxY == null) this.maxY = FatRandomFloat(ch/4, ch, 2);
 
-        ctx2.fillStyle = 'rgba(255, 255, 255, 0.8)';
-        ctx2.font = font;
-        ctx2.fillText(this.value, this.x, this.y);
+        white_text_ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+        white_text_ctx.font = font;
+        white_text_ctx.fillText(this.value, this.x, this.y);
 
-        ctx.fillStyle = '#0F0';
-        ctx.font = font;
-        ctx.fillText(this.value, this.x, this.y);
+        green_text_ctx.fillStyle = 'green';
+        green_text_ctx.font = font;
+        green_text_ctx.fillText(this.value, this.x, this.y);
 
         this.y += this.speed;
         if (this.y > this.maxY) {
@@ -76,13 +76,13 @@
     }
 
     var update = function() {
-        ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-        ctx.fillRect(0, 0, cw, ch);
-        ctx2.clearRect(0, 0, cw, ch);
+        green_text_ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+        green_text_ctx.fillRect(0, 0, cw, ch);
+        white_text_ctx.clearRect(0, 0, cw, ch);
 
         var i = fallingCharArr.length;
         while (i--) {
-            fallingCharArr[i].draw(ctx);
+            fallingCharArr[i].draw();
             var v = fallingCharArr[i];
         }
         setTimeout(function() {
