5b6c7c13d8d8808ecc94531883c021a778d751e0
peter <petermiller1986@gmail.com>
2017-11-24 23:46:59 +1030

    hide the footer until it is in position, then fade it in

--- .source/themes/thematrix/static/js/autofooter.js
+++ .source/themes/thematrix/static/js/autofooter.js
@@ -31,15 +31,21 @@ $(window).on('resize', make_sticky_footer);
 
 function make_sticky_footer() {
     var body_height = $('body').height();
+    var $about = $('#about');
 
     // add 2px for border-top, 15px for padding-top, 18px for margin-bottom
-    var footer_height = $('#about').height() + 2 + 15 + 18;
+    var footer_height = $about.height() + 2 + 15 + 18;
 
-    var original_footer_top = $('#about').offset().top;
+    var original_footer_top = $about.offset().top;
     var js_footer_push_top = $('#js_footer_push').offset().top;
     var js_footer_push_height = body_height - js_footer_push_top - footer_height;
-    if (js_footer_push_height < 0) return; // stickyness
+    if (js_footer_push_height > 0) { // stickyness
 
-    // use padding-top to set the height
-    $('#js_footer_push').css('padding-top', js_footer_push_height);
+        // use padding-top to set the height
+        $('#js_footer_push').css('padding-top', js_footer_push_height);
+    }
+
+    // show footer once it is in position, to prevent it from looking like it is
+    // bouncing around the page
+    $about.removeClass('invisible').hide().fadeIn('slow');
 }
