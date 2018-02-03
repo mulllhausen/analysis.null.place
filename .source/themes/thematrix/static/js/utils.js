8a89aed8baa21143449d77923caad2d6937de797
peter <petermiller1986@gmail.com>
2018-02-03 12:30:21 +1030

    new content layout using floating divs not a table

--- .source/themes/thematrix/static/js/utils.js
+++ .source/themes/thematrix/static/js/utils.js
@@ -5,22 +5,85 @@ function unixtime(date_obj) {
     return Math.round(date_obj.getTime() / 1000);
 }
 
-function addEvent(object, types, callback) {
-    if (object == null || typeof(object) == 'undefined') return;
+Element.prototype.isNodeList =
+Window.prototype.isNodeList =
+function() {return false;}
+
+NodeList.prototype.isNodeList =
+HTMLCollection.prototype.isNodeList =
+function() {return true;}
+
+function addEvent(element, types, callback) {
+    if (element == null || typeof(element) == 'undefined') return;
     var typesArr = types.split(',');
-    for (var i = 0; i < typesArr.length; i++) {
-        var type = typesArr[i].replace(/ /g, '');
-        if (object.addEventListener) {
-            object.addEventListener(type, callback, false);
-        } else if (object.attachEvent) {
-            object.attachEvent('on' + type, callback);
-        } else {
-            object['on' + type] = callback;
+    var elements = (element.isNodeList() ? element : [element]);
+    for (var el_i = 0; el_i < elements.length; el_i++) {
+        var el = elements[el_i];
+        for (var type_i = 0; type_i < typesArr.length; type_i++) {
+            var type = typesArr[type_i].replace(/ /g, '');
+            if (el.addEventListener) {
+                el.addEventListener(type, callback, false);
+            } else if (el.attachEvent) {
+                el.attachEvent('on' + type, callback);
+            } else {
+                el['on' + type] = callback;
+            }
         }
     }
 }
 
-// navbar open/close (mobile only)
+function triggerEvent(element, type) {
+    if ('createEvent' in document) {
+        var evt = document.createEvent('HTMLEvents');
+        evt.initEvent(type, false, true);
+        element.dispatchEvent(evt);
+    }
+    else element.fireEvent('on' + type);
+}
+
+function popup(heading, detail) {
+    document.getElementById('fullpagePopupHeading').innerText = heading;
+    document.getElementById('fullpagePopupDetail').innerText = detail;
+    document.getElementById('fullpagePopupNotification').style.display = 'table';
+    setTimeout(hidePopup, 3000);
+}
+
+function hidePopup() {
+    document.getElementById('fullpagePopupNotification').style.display = 'none';
+}
+
+function addLi2Ul(ulID, liID, errorText) {
+    var ul = document.getElementById(ulID);
+    var li = document.createElement('li');
+    li.id = liID;
+    li.appendChild(document.createTextNode(errorText));
+    ul.appendChild(li);
+}
+
+function deleteElementById(elID) {
+    var el = document.getElementById(elID);
+    if (el == null) return;
+    el.parentNode.removeChild(el);
+}
+
+function isHex(val) {
+    var regex = /^[0-9a-f]+$/gi;
+    return regex.test(val);
+}
+
+function stringIsInt(x)  {
+    return (x == parseInt(x, 10));
+}
+
+function setButtons(enable) {
+    for (var i = 1; i < arguments.length; i++) {
+        document.getElementById('btn' + arguments[i]).disabled = !enable;
+    }
+}
+
+// events for all pages
+
+// nav-menu open/close (mobile only)
 addEvent(document.getElementById('btnNavbar'), 'click', function(e) {
     var btn = e.currentTarget;
     var menu = document.getElementById('navMenu');
@@ -31,4 +94,10 @@ addEvent(document.getElementById('btnNavbar'), 'click', function(e) {
         btn.setAttribute('menu-is-collapsed', 'true');
         menu.style.display = 'none';
     }
+});
+
+// prevent css :focus from persisting after a click, but allow it to remain
+// after focus via keyboard tabbing
+addEvent(document.getElementsByTagName('button'), 'click', function(e) {
+    e.currentTarget.blur();
 });
