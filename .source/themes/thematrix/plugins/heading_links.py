094a5c7530aad0659b4e41700911260f0287f69e
peter <petermiller1986@gmail.com>
2018-07-11 09:42:38 +0930

    new plugin for heading links

---
+++ .source/themes/thematrix/plugins/heading_links.py
@@ -0,0 +1,37 @@
+if __name__ != '__main__':
+    from pelican import signals
+
+import re
+
+def process_pelican_headings(data_from_pelican):
+    if not data_from_pelican._content:
+        return
+
+    data_from_pelican._content = process_headings(data_from_pelican._content)
+
+def process_headings(html):
+    for h in xrange(1, 7):
+        pattern = re.compile(r'(<h%d>([\s\S]+?)</h%d>)' % (h, h))
+        for m in re.finditer(pattern, html):
+            find = m.group(0)
+            heading_text = m.group(2)
+            hid = re.sub('[^a-zA-Z0-9]', '_', heading_text)
+            hid = re.sub('_+', '_', hid).strip('_')
+            replace = '<h%d id="%s">%s</h%d>' % (h, hid, heading_text, h)
+            html = html.replace(find, replace)
+
+    return html
+    
+
+def register():
+    # process the content only
+    signals.content_object_init.connect(process_pelican_headings)
+
+if __name__ == '__main__':
+    test_data = '<h1>hi there</h1><h3>??sup??</h3>'
+    expected_res = '<h1 id="hi_there">hi there</h1><h3 id="sup">??sup??</h3>'
+    res = process_headings(test_data)
+    if res == expected_res:
+        print "pass"
+    else:
+        print "fail"
