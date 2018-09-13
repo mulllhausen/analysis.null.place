e45dab047cf44a3b0728231c54a0c003338f7071
peter <petermiller1986@gmail.com>
2018-09-14 07:58:41 +0930

    delete pre-merge js files

--- theme/js/cookie-warning-notice.js
+++
@@ -1,1 +0,0 @@
-if(Retrieve("cookie-warning-accepted")!="ok"){document.querySelector(".cookie-warning-notice").style.display="block";addEvent(document.querySelector(".cookie-warning-notice button#ok"),"click",function(){Save("cookie-warning-accepted","ok");document.querySelector(".cookie-warning-notice").style.display="none"})};
