2851a1964c12d2f36fffcaa5c2f8241ec77b7015
peter <petermiller1986@gmail.com>
2018-09-13 09:49:44 +0930

    add a permanent link to the cookie page in the footer

--- .source/themes/thematrix/static/js/cookie-warning-notice.js
+++ .source/themes/thematrix/static/js/cookie-warning-notice.js
@@ -1,11 +1,14 @@
-if (Retrieve('cookie-warning-accepted') != 'ok') {
-    document.querySelector('.cookie-warning-notice').style.display = 'block';
-    addEvent(
-        document.querySelector('.cookie-warning-notice button#ok'),
-        'click',
-        function () {
-            Save('cookie-warning-accepted', 'ok');
-            document.querySelector('.cookie-warning-notice').style.display = 'none';
-        }
-    );
-}
+addEvent(window, 'load', function () {
+    if (Retrieve('cookie-warning-accepted') != 'ok') {
+        document.querySelector('.cookie-warning-notice').style.display = 'block';
+        addEvent(
+            document.querySelector('.cookie-warning-notice button#ok'),
+            'click',
+            function () {
+                Save('cookie-warning-accepted', 'ok');
+                document.querySelector('.cookie-warning-notice').style.display =
+                'none';
+            }
+        );
+    }
+});
