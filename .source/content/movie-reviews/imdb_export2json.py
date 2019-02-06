4beaea439ceacd33cdcc37ed9f363503feec8449
peter <petermiller1986@gmail.com>
2019-02-07 08:50:23 +1030

    get thumbnails from imdb

--- .source/content/movie-reviews/imdb_export2json.py
+++ .source/content/movie-reviews/imdb_export2json.py
@@ -6,8 +6,10 @@ convert the imdb 'your ratings' > export file from csv to json
 """
 import os
 import csv
-import json
 from datetime import datetime
+import re
+import requests
+import time
 
 pwd = os.path.dirname(os.path.realpath(__file__))
 
@@ -36,6 +38,30 @@ def parseRating(ratingStr):
         rating = int(rating)
     return rating
 
+def getThumbURL(movieTitle, imdbID):
+    time.sleep(1) # do not spam imdb with requests
+    r = requests.get("https://imdb.com/title/%s/" % imdbID)
+    # output to file for debugging
+    #with open("/tmp/x", "w") as f:
+    #    f.write(r.content)
+    #exit()
+    try:
+        m1 = re.search("<div class=\"poster\">(.*?)</div>", r.content, re.DOTALL)
+        chunk = m1.group(1)
+        m2 = re.search("src=\"(.*?)\"", chunk, re.DOTALL)
+        thumbURL = m2.group(1)
+
+        # now change the image to choose one with the correct dimensions
+        # change this: @._V1_UX182_CR0,0,182,268_AL_.jpg
+        # to this:     @._V1_UY209_CR1,0,140,209_AL_.jpg
+        thumbURL = thumbURL.replace(
+            "@._V1_UX182_CR0,0,182,268_AL_.jpg", "@._V1_UY209_CR1,0,140,209_AL_.jpg"
+        )
+        return thumbURL
+    except:
+        print "failed to get thumbnail for \"%s\"" % movieTitle
+        return ""
+
 with open("%s/ratings.csv" % pwd, "r") as f:
     reader = csv.reader(f)
     for (r, row) in enumerate(reader):
@@ -53,6 +79,7 @@ with open("%s/ratings.csv" % pwd, "r") as f:
         if row[5] != "movie":
             print "skip row %s (\"%s\") since is not a movie, it is \"%s\"" % \
             (r, row[3], row[5])
+            continue
 
         ratings.append({
             "rating-date": datetime.strptime(row[2], "%Y-%m-%d"),
@@ -72,13 +99,34 @@ ratings = sorted(
     key = lambda movie: movie["rating-date"]
 )
 
+ratings_intermediate = []
 for movie in ratings:
     del movie["rating-date"]
-
-# print json.dumps(ratings, indent = 4, sort_keys = False, ensure_ascii = True)
+    thumbnailURL = getThumbURL(movie["title"], movie["IMDBID"])
+    ratings_intermediate.append("""
+        {
+            "title": "%s",
+            "year": %s,
+            "rating": %s,
+            "spoilers": false,
+            "thumbnail": "%s",
+            "reviewTitle": "",
+            "review": "%s",
+            "IMDBID": "%s",
+            "genres": ["%s"]
+        }""" % (
+            movie["title"],
+            movie["year"],
+            movie["rating"],
+            thumbnailURL,
+            movie["review"],
+            movie["IMDBID"],
+            "\", \"".join(movie["genres"])
+        )
+    )
 
 outfile = "%s/ratings.json" % pwd
 print "output saved to file %s" % outfile
 
 with open(outfile, "w") as f:
-    json.dump(ratings, f, indent = 4, sort_keys = False, ensure_ascii = True)
+    f.write("[%s\n]" % ",".join(ratings_intermediate)) 
