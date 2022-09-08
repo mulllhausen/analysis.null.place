09c7ec62166f9cfeb5bb583c48d48f4d8ab67a1e
Peter Miller <pmiller@biscit.com.au>
2022-09-08 20:11:55 +0930

    publish

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var latestCache = '2022-09-08_19:15:14';
+var latestCache = '2022-09-08_20:11:06';
 var mandatoryAssets = [
     // common assets for all pages
     'https://analysis.null.place/theme/css/thematrix.css?hash=pkRr8e',
